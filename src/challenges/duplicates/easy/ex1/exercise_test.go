6c9b0b46725a08c9c6e05e80f9e5d9b09c95fbd7
Carson <jackparadise520a@gmail.com>
2023-11-16 09:47:07 +0800

    目錄結構調整

--- src/challenges/duplicates/easy/ex1/exercise_test.go
+++ src/challenges/duplicates/easy/ex1/exercise_test.go
@@ -2,10 +2,11 @@ package ex1_test
 
 import (
 	"fmt"
-	"go_exercise/challenges/easy/duplicates/ex1"
+	"go_exercise/challenges/duplicates/easy/ex1"
 	"sort"
 )
 
+// 找出切片(可能未排序)有重複的數字
 func ExampleFindDuplicates() {
 	arr := []int{1, 3, 6, 3, 5, 6, 7}
 	fmt.Println(ex1.FindDuplicates(arr))
@@ -13,7 +14,7 @@ func ExampleFindDuplicates() {
 	// [3 6]
 }
 
-// 將arr排序，必且找出裡面有重複的數字
+// 將arr排序，並且找出裡面有重複的數字
 func ExampleFindDuplicatesWithOptions() {
 	arr := []int{1, 3, 6, 3, 5, 6, 7}
 	duplicates := ex1.FindDuplicatesWithOptions(arr, func(s []int) {
